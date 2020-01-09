c1adf90018040a5d8dc2ba4bde4efa5476494274
Zachary Baum <zac_baum@hotmail.com>
2020-01-09 19:29:43 +0000

    Update debug plots

--- callbacks.py
+++ callbacks.py
@@ -3,6 +3,7 @@ import keras
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('AGG')
+import numpy as np
 
 class Prediction_Plotter(keras.callbacks.Callback):
     def __init__(self, X_val, Y_val, fname_tag, debug=False):
@@ -18,7 +19,7 @@ class Prediction_Plotter(keras.callbacks.Callback):
         pred = self.model.predict(self.X_val)
         
         assert(self.Y_val.shape[0] == pred.shape[0])
-        for batch_id in range(0, pred.shape[0], 25):
+        for batch_id in range(0, pred.shape[0], 10):
             
             plt.clf()
             fig = plt.figure()
@@ -50,14 +51,14 @@ class Prediction_Plotter(keras.callbacks.Callback):
 
                 ax.scatter(x_pred, y_pred, z_pred, c='g', marker='.')
 
-                ax.set_xlim([-1, 1])
-                ax.set_ylim([-1, 1])
-                ax.set_zlim([-1, 1])
+                mn = abs(np.min(pred[batch_id]))
+                mx = np.max(pred[batch_id])
+                limit = max(mn, mx)
+
+                ax.set_xlim([limit, -limit])
+                ax.set_ylim([limit, -limit])
+                ax.set_zlim([limit, -limit])
 
                 plt.show()
-                plt.savefig('./results/' + str(self.fname_tag) + '_id_' + str(batch_id + 1) + '_epoch_' + str(epoch + 1) + '_reg-scatter-PRED_ONLY.png', dpi=250)
+                plt.savefig('./results/PRED_ZOOMED-' + str(self.fname_tag) + '_id_' + str(batch_id + 1) + '_epoch_' + str(epoch + 1) + '_reg-scatter.png', dpi=250)
                 plt.close()
-
-                print("GTRU:\n", self.Y_val[batch_id])
-                print("PRED:\n", pred[batch_id])
-                print()
