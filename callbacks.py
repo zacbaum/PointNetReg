36dfc861fdfe4595e556792fb4e1c6d7d460509a
Zachary Baum <zac_baum@hotmail.com>
2020-01-14 09:18:53 +0000

    Add additional debug plots, add loss plotter

--- callbacks.py
+++ callbacks.py
@@ -6,9 +6,9 @@ matplotlib.use('AGG')
 import numpy as np
 
 class Prediction_Plotter(keras.callbacks.Callback):
-    def __init__(self, X_val, Y_val, fname_tag, debug=False):
-        self.X_val = X_val
-        self.Y_val = Y_val
+    def __init__(self, X, Y, fname_tag, debug=False):
+        self.X = X
+        self.Y = Y
         self.fname_tag = fname_tag
         self.debug = debug
 
@@ -16,18 +16,18 @@ class Prediction_Plotter(keras.callbacks.Callback):
         pass
 
     def on_epoch_end(self, epoch, logs={}):    
-        pred = self.model.predict(self.X_val)
-        
-        assert(self.Y_val.shape[0] == pred.shape[0])
-        for batch_id in range(0, pred.shape[0], 10):
+        pred = self.model.predict(self.X)
+
+        assert(self.Y.shape[0] == pred.shape[0])
+        for batch_id in range(0, pred.shape[0], int(pred.shape[0] / 3)):
             
             plt.clf()
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
 
-            x_true = [i[0] for i in self.Y_val[batch_id]]
-            y_true = [i[1] for i in self.Y_val[batch_id]]
-            z_true = [i[2] for i in self.Y_val[batch_id]]
+            x_true = [i[0] for i in self.X[0][batch_id]]
+            y_true = [i[1] for i in self.X[0][batch_id]]
+            z_true = [i[2] for i in self.X[0][batch_id]]
 
             x_pred = [i[0] for i in pred[batch_id]]
             y_pred = [i[1] for i in pred[batch_id]]
@@ -45,6 +45,7 @@ class Prediction_Plotter(keras.callbacks.Callback):
             plt.close()
 
             if self.debug:
+                
                 plt.clf()
                 fig = plt.figure()
                 ax = fig.add_subplot(111, projection='3d')
@@ -62,3 +63,53 @@ class Prediction_Plotter(keras.callbacks.Callback):
                 plt.show()
                 plt.savefig('./results/PRED_ZOOMED-' + str(self.fname_tag) + '_id_' + str(batch_id + 1) + '_epoch_' + str(epoch + 1) + '_reg-scatter.png', dpi=250)
                 plt.close()
+
+
+                plt.clf()
+                fig = plt.figure()
+                ax = fig.add_subplot(111, projection='3d')
+
+                x_move = [i[0] for i in self.X[1][batch_id]]
+                y_move = [i[1] for i in self.X[1][batch_id]]
+                z_move = [i[2] for i in self.X[1][batch_id]]
+
+                ax.scatter(x_move, y_move, z_move, c='r', marker='.')
+                ax.scatter(x_pred, y_pred, z_pred, c='g', marker='.')
+            
+                ax.set_xlim([-1, 1])
+                ax.set_ylim([-1, 1])
+                ax.set_zlim([-1, 1])
+
+                plt.show()
+                plt.savefig('./results/MOVE_PRED-' + str(self.fname_tag) + '_id_' + str(batch_id + 1) + '_epoch_' + str(epoch + 1) + '_reg-scatter.png', dpi=250)
+                plt.close()
+
+class PlotLosses(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        
+        self.losses = []
+        self.val_losses = []
+        
+        self.fig = plt.figure()
+        
+        self.logs = []
+
+    def on_epoch_end(self, epoch, logs={}):
+
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.i += 1
+        f, ax1 = plt.subplots(1, 1, sharex=True)
+                
+        ax1.plot(self.x, self.losses, label="loss")
+        ax1.plot(self.x, self.val_losses, label="validation loss")
+        ax1.legend()
+        
+        plt.show()
+        plt.yscale('log')
+        plt.savefig('live_metrics.png', dpi=250)
+        plt.close()
