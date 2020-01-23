2582b5a2f52d3cd6ad4463a2f3494bfb3b1080d0
Zachary Baum <zac_baum@hotmail.com>
2020-01-23 11:42:43 +0000

    Add loss specific LRs, and LR reducer

--- train_cls.py
+++ train_cls.py
@@ -1,24 +1,23 @@
-from callbacks import Prediction_Plotter, PlotLosses
-from data_loader import DataGenerator
-from keras.optimizers import SGD, Adam
-from keras.callbacks import ModelCheckpoint
-from losses import sorted_mse_loss, chamfer_loss, gmm_nll_loss
-from model import ConditionalTransformerNet, TPSTransformNet
-from mpl_toolkits.mplot3d import Axes3D
 import h5py
 import keras
 import matplotlib
 import matplotlib.pyplot as plt
-import sys
+import numpy as np
 import os
-from datetime import datetime
-os.environ["CUDA_VISIBLE_DEVICES"]=sys.argv[2]
-os.environ['KERAS_BACKEND'] = 'tensorflow'
+import sys
 import tensorflow as tf
 import tensorflow_probability as tfp
-tfd = tfp.distributions
+from callbacks import Prediction_Plotter, PlotLosses
+from data_loader import DataGenerator
+from datetime import datetime
 from keras import backend as K
-import numpy as np
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import SGD, Adam
+from losses import chamfer_loss, gmm_nll_loss
+from model import ConditionalTransformerNet, TPSTransformNet
+from mpl_toolkits.mplot3d import Axes3D
+os.environ["CUDA_VISIBLE_DEVICES"]=sys.argv[2]
+tfd = tfp.distributions
 
 matplotlib.use('AGG')
 
@@ -43,12 +42,22 @@ def main():
 	batch_size = 3 * 32
 
 	loss_name = str(sys.argv[1])
-	if loss_name == 'sorted_mse_loss': loss = sorted_mse_loss
-	if loss_name == 'chamfer_loss': loss = chamfer_loss
-	if loss_name == 'gmm_nll_loss': loss = gmm_nll_loss
+	loss_func = None
+	metrics = None
+	
+	if loss_name == 'chamfer_loss':
+		loss_func = chamfer_loss
+		learning_rate = 1e-2
+	
+	elif loss_name == 'gmm_nll_loss':
+		loss_func = gmm_nll_loss
+		metrics = [chamfer_loss]
+		learning_rate = 1e-4
+	
+	else:
+		loss_func = gmm_nll_loss
 
 	train = DataGenerator(train_file, batch_size, deform=True)
-	
 	train_data = []     # store all the generated data batches
 	train_labels = []   # store all the generated ground_truth batches
 	max_iter = 1        # maximum number of iterations, in each iteration one batch is generated; the proper value depends on batch size and size of whole data
@@ -59,10 +68,9 @@ def main():
 		i += 1
 		if i == max_iter:
 			break
+
 	fixed_len = train_data[0][0].shape[1]
 	moving_len = train_data[0][1].shape[1]
-
-	assert fixed_len == moving_len, 'Lengths not consistent'
 	num_points = fixed_len
 
 	first_train_X = train_data[0]
@@ -70,7 +78,6 @@ def main():
 	Prediction_Plot_Train = Prediction_Plotter(first_train_X, first_train_Y, loss_name + '-train')
 
 	val = DataGenerator(test_file, batch_size, deform=True)
-
 	val_data = []     # store all the generated data batches
 	val_labels = []   # store all the generated ground_truth batches
 	max_iter = 1      # maximum number of iterations, in each iteration one batch is generated; the proper value depends on batch size and size of whole data
@@ -90,12 +97,13 @@ def main():
 
 	logdir = "./logs/CTN_" + datetime.now().strftime("%Y%m%d-%H%M%S")
 	checkpointer = ModelCheckpoint(filepath='./logs/CTN_Model_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.hdf5', verbose=0, save_best_only=True)
+	lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=1e-10)
 
 	model = ConditionalTransformerNet(num_points)
-	learning_rate = 5e-2
 	opt = Adam(lr=learning_rate)
 	model.compile(optimizer=opt,
-				  loss=chamfer_loss)
+				  loss=loss_func,
+				  metrics=metrics)
 
 	if not os.path.exists('./results/'):
 		os.mkdir('./results/')
@@ -110,7 +118,7 @@ def main():
 								  epochs=num_epochs,
 								  validation_data=val.generator(),
 								  validation_steps=num_val // batch_size,
-								  callbacks=[Prediction_Plot_Train, Prediction_Plot_Val, LossPlotter, checkpointer],
+								  callbacks=[Prediction_Plot_Train, Prediction_Plot_Val, LossPlotter, checkpointer, lr_reducer],
 								  verbose=2)
 	model.save('./results/CTN-' + loss_name + '.h5')
 	name = ''
