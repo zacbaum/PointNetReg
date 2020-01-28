24818fdbd6cf67d471d136a40658e6f338a804eb
Zachary Baum <zac_baum@hotmail.com>
2020-01-28 09:36:30 +0000

    Update train_cls.py
    Refactor for use with new architecture.

--- train_cls.py
+++ train_cls.py
@@ -6,7 +6,6 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
-import tensorflow_probability as tfp
 import wandb
 from callbacks import Prediction_Plotter, PlotLosses
 from data_loader import DataGenerator
@@ -20,7 +19,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from wandb.keras import WandbCallback
 matplotlib.use('AGG')
 os.environ["CUDA_VISIBLE_DEVICES"]=sys.argv[2]
-tfd = tfp.distributions
 
 def plot_results(attribute, output, filename):
 	plt.figure(figsize=(16,10))
@@ -43,31 +41,23 @@ def main():
 	if not os.path.exists('./logs' + str(sys.argv[2]) + '/'):
 		os.mkdir('./logs' + str(sys.argv[2]) + '/')
 
-	num_epochs = 500
-	batch_size = 32
-	scale = 1000
+	batch_size = 80
+	scale = 1
 
 	loss_name = str(sys.argv[1])
 	loss_func = None
 	metrics = None
-	
+
+	wandb.init(project = "ctn-chamfer", name = 'cd 1e-3 do0.0 bn0')
+
 	if loss_name == 'chamfer_loss':
 		learning_rate = float(sys.argv[3])
-		wandb.init(project = "ctn")
-		wandb.init(config = {"learning_rate_init":learning_rate,
-							 "loss_name":loss_name})
 		loss_func = chamfer_loss
 	
 	if loss_name == 'gmm_nll_loss':
-
 		learning_rate = float(sys.argv[3])
 		covariance_matrix_diag = float(sys.argv[4])
 		mix_param = float(sys.argv[5])
-		wandb.init(project = "ctn")
-		wandb.init(config = {"learning_rate_init":learning_rate,
-						     "covariance_matrix_diag":covariance_matrix_diag,
-						     "mix_param":mix_param,
-						     "loss_name":loss_name})
 		loss_func = gmm_nll_loss(covariance_matrix_diag, mix_param)
 		metrics = [chamfer_loss]
 
@@ -88,6 +78,7 @@ def main():
 
 	fixed_len = train_data[0][0].shape[1]
 	moving_len = train_data[0][1].shape[1]
+	assert (fixed_len == moving_len)
 	num_points = fixed_len
 
 	first_train_X = train_data[0]
@@ -124,15 +115,11 @@ def main():
 								   verbose=0,
 								   save_best_only=True)
 
-	lr_reducer = ReduceLROnPlateau(monitor='val_loss',
-								   factor=0.5,
-								   patience=10,
-								   min_lr=1e-10)
 	LossPlotter = PlotLosses()
 
-	model = ConditionalTransformerNet(num_points)
-	opt = Adam(lr=learning_rate)
-	model.compile(optimizer=opt,
+	model = ConditionalTransformerNet(num_points, dropout=0.0, batch_norm=False)
+	optimizer = Adam(lr=learning_rate)
+	model.compile(optimizer=optimizer,
 				  loss=loss_func,
 				  metrics=metrics)
 
@@ -140,16 +127,16 @@ def main():
 	num_train = f['data'].shape[0]
 	f = h5py.File(test_file, mode='r')
 	num_val = f['data'].shape[0]
+
 	history = model.fit_generator(train.generator(),
 								  steps_per_epoch=num_train // batch_size,
-								  epochs=num_epochs,
+								  epochs=500,
 								  validation_data=val.generator(),
 								  validation_steps=num_val // batch_size,
 								  callbacks=[Prediction_Plot_Train, 
-								  			 Prediction_Plot_Val, 
-								  			 checkpointer, 
-								  			 lr_reducer,
-								  			 WandbCallback()],
+											 Prediction_Plot_Val, 
+											 checkpointer,
+											 WandbCallback()],
 								  verbose=2)
 
 	model.save('./results' + str(sys.argv[2]) + '/CTN-' + loss_name + '.h5')
