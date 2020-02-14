10100ad3ee094d4f9d3c66e9bb85aafcb5e04b9a
Zachary Baum <zac_baum@hotmail.com>
2020-02-14 11:09:40 +0000

    Use two-way variational distance

--- losses.py
+++ losses.py
@@ -19,36 +19,41 @@ def chamfer_loss(y_true, y_pred):
 	batched_losses = tf.map_fn(lambda x: chamfer_distance(x[0], x[1]), (y_true, y_pred), dtype=tf.float32)
 	return K.mean(tf.stack(batched_losses))
 
-def variational_distance(y_true, y_pred):
-	sigma = 1.0
-	N = tf.cast(K.int_shape(y_pred)[0], dtype=tf.float32)
+def variational_distance(A, B, L, sigma=1.0):
+	N = tf.cast(K.int_shape(L)[0], dtype=tf.float32)
 
-	# Part 1 - D(a||a')
-	a = tf.reduce_sum(tf.square(y_true), axis=1)
+	# Part 1A - D(a||a')
+	a = tf.reduce_sum(tf.square(B), axis=1)
 	a = tf.reshape(a, [-1, 1])
-	D_top = tf.sqrt(a - 2 * tf.matmul(y_true, tf.transpose(y_true)) + tf.transpose(a))
-	# Part 1b - Rest of top
+	#D_top = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(B)) + tf.transpose(a))
+	D_top = a - 2 * tf.matmul(B, tf.transpose(B)) + tf.transpose(a)
+	# Part 1B - SUM( e^(-D(a||a') / N )
 	D_top = tf.truediv(tf.square(D_top), (2 * sigma**2))
-	D_top = tf.clip_by_value(tf.exp(-D_top), 1e-10, 1)
-	D_top = tf.clip_by_value(tf.truediv(D_top, N), 1e-10, 1)
+	D_top = tf.clip_by_value(tf.exp(-D_top), 1e-15, 1e15)
+	D_top = tf.truediv(D_top, N)
 
-	# Part 2 - D(a||b)
-	a = tf.reduce_sum(tf.square(y_true), axis=1)
+	# Part 2A - D(a||b)
+	a = tf.reduce_sum(tf.square(B), axis=1)
 	a = tf.reshape(a, [-1, 1])
-	b = tf.reduce_sum(tf.square(y_pred), axis=1)
+	b = tf.reduce_sum(tf.square(A), axis=1)
 	b = tf.reshape(b, [1, -1])
-	D_bottom = tf.sqrt(a - 2 * tf.matmul(y_true, tf.transpose(y_pred)) + b)
-	# Part 2b - Rest of bottom
+	#D_bottom = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(A)) + b)
+	D_bottom = a - 2 * tf.matmul(B, tf.transpose(A)) + b
+	# Part 2B - SUM( e^(-D(a||b) / N )
 	D_bottom = tf.truediv(tf.square(D_bottom), (2 * sigma**2))
-	D_bottom = tf.clip_by_value(tf.exp(-D_bottom), 1e-10, 1)
-	D_bottom = tf.clip_by_value(tf.truediv(D_bottom, N), 1e-10, 1)
-
+	D_bottom = tf.clip_by_value(tf.exp(-D_bottom), 1e-15, 1e15)
+	D_bottom = tf.truediv(D_bottom, N)
+	
+	# Part 3 - SUM( log( 2A / 2B ) ) / N
 	main_div = tf.log(tf.reduce_sum(D_top, axis=1)) - tf.log(tf.reduce_sum(D_bottom, axis=1))
-	return K.abs(tf.reduce_sum(main_div) / N)
+	main_div = tf.reduce_sum(main_div) / N
+	return K.abs(main_div)
 
 def variational_loss(y_true, y_pred):
-	batched_losses = tf.map_fn(lambda x: variational_distance(x[0], x[1]), (y_true, y_pred), dtype=tf.float32)
-	return K.mean(tf.stack(batched_losses))
+	batched_losses_AB = tf.map_fn(lambda x: variational_distance(x[0], x[1], x[2]), (y_true, y_pred, y_pred), dtype=tf.float32)
+	batched_losses_BA = tf.map_fn(lambda x: variational_distance(x[0], x[1], x[2]), (y_pred, y_true, y_pred), dtype=tf.float32)
+	return K.mean(tf.stack([tf.stack(batched_losses_AB),
+							tf.stack(batched_losses_BA)]))
 
 def gmm_nll_loss(covariance_matrix_diag, mix_param_val):
 
