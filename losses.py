4fad11ffaf5b6fb3f3edf926f977d52f6579d2a6
Zachary Baum <zac_baum@hotmail.com>
2020-03-02 17:08:37 +0000

    Add combined chamfer/variational loss
    Testing to see if this helps with the 'shrinking' encountered in the displaced nets

--- losses.py
+++ losses.py
@@ -25,10 +25,11 @@ def variational_distance(A, B, L, sigma=1.0):
 	# Part 1A - D(a||a')
 	a = tf.reduce_sum(tf.square(B), axis=1)
 	a = tf.reshape(a, [-1, 1])
-	D_top = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(B)) + tf.transpose(a))
+	#D_top = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(B)) + tf.transpose(a))
+	D_top = a - 2 * tf.matmul(B, tf.transpose(B)) + tf.transpose(a)
 	# Part 1B - SUM( e^(-D(a||a') / N )
 	D_top = tf.truediv(tf.square(D_top), (2 * sigma**2))
-	D_top = tf.clip_by_value(tf.exp(-D_top), 1e-15, 1e15)
+	D_top = tf.clip_by_value(tf.exp(-D_top), 1e-10, 1e10)
 	D_top = tf.truediv(D_top, N)
 
 	# Part 2A - D(a||b)
@@ -36,14 +37,15 @@ def variational_distance(A, B, L, sigma=1.0):
 	a = tf.reshape(a, [-1, 1])
 	b = tf.reduce_sum(tf.square(A), axis=1)
 	b = tf.reshape(b, [1, -1])
-	D_bottom = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(A)) + b)
+	#D_bottom = tf.sqrt(a - 2 * tf.matmul(B, tf.transpose(A)) + b)
+	D_bottom = a - 2 * tf.matmul(B, tf.transpose(A)) + b
 	# Part 2B - SUM( e^(-D(a||b) / N )
 	D_bottom = tf.truediv(tf.square(D_bottom), (2 * sigma**2))
-	D_bottom = tf.clip_by_value(tf.exp(-D_bottom), 1e-15, 1e15)
+	D_bottom = tf.clip_by_value(tf.exp(-D_bottom), 1e-10, 1e10)
 	D_bottom = tf.truediv(D_bottom, N)
 	
 	# Part 3 - SUM( log( 2A / 2B ) ) / N
-	main_div = tf.log(tf.reduce_sum(D_top, axis=1)) - tf.log(tf.reduce_sum(D_bottom, axis=1))
+	main_div = tf.math.log(tf.reduce_sum(D_top, axis=1)) - tf.math.log(tf.reduce_sum(D_bottom, axis=1))
 	main_div = tf.reduce_sum(main_div) / N
 	return K.abs(main_div)
 
@@ -52,6 +54,9 @@ def variational_loss(y_true, y_pred):
 	batched_losses_BA = tf.map_fn(lambda x: variational_distance(x[0], x[1], x[2]), (y_pred, y_true, y_pred), dtype=tf.float32)
 	return K.mean(tf.stack([tf.stack(batched_losses_AB),
 							tf.stack(batched_losses_BA)]))
+
+def cham_var_loss(y_true, y_pred):
+	return K.mean(tf.stack([variational_loss(y_true, y_pred), chamfer_loss(y_true, y_pred)]))
 
 def gmm_nll_loss(covariance_matrix_diag, mix_param_val):
 
