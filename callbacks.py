0853640fbba95ba2ca9f8901f602bd56f12fc945
Zachary Baum <zac_baum@hotmail.com>
2020-06-18 15:11:52 -0400

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -17,6 +17,7 @@ class Prediction_Plotter(tf.keras.callbacks.Callback):
         self.on_epoch_end(-1)
 
     def on_epoch_end(self, epoch, logs={}):
+
         pred = self.model.predict_on_batch(self.X)
 
         assert(self.Y.shape[0] == pred.shape[0])
@@ -29,24 +30,29 @@ class Prediction_Plotter(tf.keras.callbacks.Callback):
             x_true = [i[0] for i in self.Y[batch_id]]
             y_true = [i[1] for i in self.Y[batch_id]]
             z_true = [i[2] for i in self.Y[batch_id]]
-
+            '''
             x_f = [i[0] for i in self.X[0][batch_id]]
             y_f = [i[1] for i in self.X[0][batch_id]]
             z_f = [i[2] for i in self.X[0][batch_id]]
 
-            #x_m = [i[0] for i in self.X[1][batch_id]]
-            #y_m = [i[1] for i in self.X[1][batch_id]]
-            #z_m = [i[2] for i in self.X[1][batch_id]]
-            
+            x_m = [i[0] for i in self.X[1][batch_id]]
+            y_m = [i[1] for i in self.X[1][batch_id]]
+            z_m = [i[2] for i in self.X[1][batch_id]]
+
+            x_r = [i[0] for i in self.X[2][batch_id]]
+            y_r = [i[1] for i in self.X[2][batch_id]]
+            z_r = [i[2] for i in self.X[2][batch_id]]
+            '''
             x_pred = [i[0] for i in pred[batch_id]]
             y_pred = [i[1] for i in pred[batch_id]]
             z_pred = [i[2] for i in pred[batch_id]]
             
             ax.scatter(x_true, y_true, z_true, c='y', marker='.')
             ax.scatter(x_pred, y_pred, z_pred, c='g', marker='.')
-            
-            ax.scatter(x_f, y_f, z_f, c='r', marker='*', alpha=0.1)
+
+            #ax.scatter(x_f, y_f, z_f, c='r', marker='*', alpha=0.1)
             #ax.scatter(x_m, y_m, z_m, c='b', marker='*', alpha=0.1)
+            #ax.scatter(x_r, y_r, z_r, c='k', marker='*', alpha=0.1)
 
             ax.set_xlim([-1, 1])
             ax.set_ylim([-1, 1])
