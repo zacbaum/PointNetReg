c4ea5bac270effd3f0bc342d4524ed5592a482c1
Zachary Baum <zac_baum@hotmail.com>
2020-10-20 21:02:16 +0100

    remove freezing, other tweaks to parameters and training steps

--- fine_tune.py
+++ fine_tune.py
@@ -39,7 +39,6 @@ def fine_tune(
 	batch_size, 
 	learning_rate, 
 	network_type,
-	freeze=0,
 	rotate=0,
 	displace=0,
 	deform=0,
@@ -47,11 +46,15 @@ def fine_tune(
 	shuffle_points=True,
 	kept_points=2048,
 	slices=False,
-	sweeps=False,
+	sweeps=0,
+	train_split=0.7,
 	epochs=5000,
 ):
 
-	results_path = "./results-PN4D-" + network_type + "-MRUS-d" + str(displace) + "-r" + str(rotate) + "-bs" + str(batch_size) + "-lr" + str(learning_rate) + "/"
+	test_split = 0.7
+
+	#results_path = "./results-PN4D-" + network_type + "-gmm-MRUS-" + str(train_split) + "-sweeps" + str(sweeps) + "-d" + str(displace) + "-r" + str(rotate) + "-bs" + str(batch_size) + "-lr" + str(learning_rate) + "/"
+	results_path = "./results-PN4D-" + str(kept_points) + "-" + network_type + "-MRUS-" + str(train_split) + "-d" + str(displace) + "-r" + str(rotate) + "-bs" + str(batch_size) + "-lr" + str(learning_rate) + "/"
 	if not os.path.exists(results_path):
 		os.mkdir(results_path)
 
@@ -69,30 +72,27 @@ def fine_tune(
 		loss_func = gmm_nll_loss(0.001, 0.1)
 		metric = [chamfer_loss_batch]
 
-	if not os.path.exists("./mrus/prostates.npy") or not os.path.exists(
-		"./mrus/prostate_metrics.npy"
-	):
+	if not os.path.exists("./mrus/prostates_volumes.npy") or not os.path.exists("./mrus/prostate_metrics_volumes.npy"):
 		all_prostates, _ = get_mr_us_data(
 			"./mrus/us_labels_resampled800_post3.h5",
 			"./mrus/mr_labels_resampled800_post3.h5",
 		)
-		np.save("./mrus/prostates.npy", all_prostates)
-		np.save("./mrus/prostate_metrics.npy", metrics)
+		np.save("./mrus/prostates_volumes.npy", all_prostates)
+		np.save("./mrus/prostate_metrics_volumes.npy", metrics)
 	else:
-		all_prostates = np.load("./mrus/prostates.npy", allow_pickle=True)
-		metrics = np.load("./mrus/prostate_metrics.npy", allow_pickle=True)
+		all_prostates = np.load("./mrus/prostates_volumes.npy", allow_pickle=True)
+		metrics = np.load("./mrus/prostate_metrics_volumes.npy", allow_pickle=True)
 
 	max_iters = len(all_prostates)
-	split = 0.7
 
 	X1 = []
 	X2 = []
-	for i in range(0, int(split * max_iters)):
+	for i in range(0, int(train_split * max_iters)):
 			
 		fixed_prostate = all_prostates[i][0][0]
 		moving_prostate = all_prostates[i][0][1]
 
-		# Make each data the Fixed, Moving, Moved
+		# Make each data the Fixed, Moving
 		X1.append(np.array(fixed_prostate))
 		X2.append(np.array(moving_prostate))
 
@@ -100,12 +100,12 @@ def fine_tune(
 
 	X1 = []
 	X2 = []
-	for i in range(int(split * max_iters), max_iters):
+	for i in range(int(test_split * max_iters), max_iters):
 
 		fixed_prostate = all_prostates[i][0][0]
 		moving_prostate = all_prostates[i][0][1]
 
-		# Make each data the Fixed, Moving, Moved
+		# Make each data the Fixed, Moving
 		X1.append(np.array(fixed_prostate))
 		X2.append(np.array(moving_prostate))
 
@@ -125,25 +125,6 @@ def fine_tune(
 		sweeps=sweeps,
 	)
 
-	trn_data = []  # store all the generated data batches
-	trn_labels = []  # store all the generated ground_truth batches
-	max_iter = 1  # maximum number of iterations, in each iteration one batch is generated; the proper value depends on batch size and size of whole data
-	i = 0
-	for d, l in train:
-		trn_data.append(d)
-		trn_labels.append(l)
-		i += 1
-		if i == max_iter:
-			break
-
-	first_trn_X = trn_data[0]
-	first_trn_Y = trn_labels[0]
-	Prediction_Plot_Trn = Prediction_Plotter(
-		first_trn_X,
-		first_trn_Y,
-		results_path + loss_name + "-train",
-	)
-
 	test = DataGenerator(
 		X_test,
 		batch_size,
@@ -156,6 +137,25 @@ def fine_tune(
 		kept_points=kept_points,
 		slices=slices,
 		sweeps=sweeps,
+	)
+
+	trn_data = []  # store all the generated data batches
+	trn_labels = []  # store all the generated ground_truth batches
+	max_iter = 1  # maximum number of iterations, in each iteration one batch is generated; the proper value depends on batch size and size of whole data
+	i = 0
+	for d, l in train:
+		trn_data.append(d)
+		trn_labels.append(l)
+		i += 1
+		if i == max_iter:
+			break
+	
+	first_trn_X = trn_data[0]
+	first_trn_Y = trn_labels[0]
+	Prediction_Plot_Trn = Prediction_Plotter(
+		first_trn_X,
+		first_trn_Y,
+		results_path + loss_name + "-trn",
 	)
 
 	val_data = []  # store all the generated data batches
@@ -178,30 +178,27 @@ def fine_tune(
 	)
 
 	wandb.init(
-		project="fpt-journal",
-		name="PN4D-" + network_type + " MRUS w/ d" + str(displace) + " r" + str(rotate) + " bs" + str(batch_size) + " lr" + str(learning_rate),
+		project="fpt-journal-volumes",
+		#name="PN4D-" + network_type + " MRUS " + str(train_split) + " gmm sweeps" + str(sweeps) + " w/ d" + str(displace) + " r" + str(rotate) + " bs" + str(batch_size) + " lr" + str(learning_rate),
+		name="PN4D-" + str(kept_points) + "-" + network_type + " MRUS " + str(train_split) + " w/ d" + str(displace) + " r" + str(rotate) + " bs" + str(batch_size) + " lr" + str(learning_rate),
 		reinit=True,
-		#resume=RUN_ID,
+		#resume='gkti67bl',
 	)
-
-	fixed_len = trn_data[0][0].shape[1]
-	moving_len = trn_data[0][1].shape[1]
-	assert fixed_len == moving_len
-	num_points = fixed_len
 
 	if network_type == 'fpt':
 		model = FreePointTransformer(
-			num_points,
+			kept_points,
 			dims=4,
 			skips=False,
 		)
 	if network_type == 'tps':
 		model = TPSTransformNet(
-			num_points,
+			kept_points,
 			dims=4,
 			tps_features=27,
 			sigma=1.0,
 		)
+
 	init_epoch = 0
 	'''
 	model = load_model(
@@ -210,53 +207,19 @@ def fine_tune(
 	)
 	initial_epoch = wandb.run.step
 	'''
-	if freeze == 1:  # Freeze everything except output
-		trainable_layers = ["conv1d_17"]
-		for layer in model.layers:
-			for name in trainable_layers:
-				if name in layer.name:
-					break
-				else:
-					layer.trainable = False
-			else:
-				continue
-			break
-
-	if freeze == 2:  # Freeze only the pointnet
-		trainable_layers = [
-			"conv1d_12",
-			"conv1d_13",
-			"conv1d_14",
-			"conv1d_15",
-			"conv1d_16",
-			"conv1d_17",
-		]
-		for layer in model.layers:
-			for name in trainable_layers:
-				if name in layer.name:
-					break
-				else:
-					layer.trainable = False
-			else:
-				continue
-			break
-
-	for layer in model.layers:
-		print(layer.name, layer.trainable)
-
 	optimizer = Adam(lr=learning_rate)
 	model.compile(optimizer=optimizer, loss=loss_func, metrics=metric)
 
 	history = model.fit_generator(
 		train,
-		steps_per_epoch=int(split * max_iters) // batch_size,
+		steps_per_epoch=int(train_split * max_iters) // batch_size,
 		epochs=epochs,
 		initial_epoch=init_epoch,
 		validation_data=test,
 		callbacks=[
-			Prediction_Plot_Trn, 
+			Prediction_Plot_Trn,
 			Prediction_Plot_Val, 
-			WandbCallback(log_weights=True)
+			WandbCallback(log_weights=True),
 		],
 		verbose=2,
 	)
@@ -268,7 +231,65 @@ if __name__ == "__main__":
 	learning_rate = float(sys.argv[3])
 	batch_size = int(sys.argv[4])
 
-	# batch size, learning rate, network_type, freeze, rotate, displace, deform, shuffle, shuff_pts, keep, slices, sweeps, eps
-	fine_tune(batch_size, learning_rate, 'fpt', 0,  0, 0, 0, True, True, 2048, False, False, 2000)
-	fine_tune(batch_size, learning_rate, 'fpt', 0,  0, 1, 0, True, True, 2048, False, False, 2000)
-	fine_tune(batch_size, learning_rate, 'fpt', 0, 45, 1, 0, True, True, 2048, False, False, 2000)
+	# batch size, learning rate, network_type, rotate, displace, deform, shuffle, shuff_pts, keep, slices, sweeps, train_split, epochs
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2**13, False, 0, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2**13, False, 0, 0.7, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2**12, False, 0, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2**12, False, 0, 0.7, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2**11, False, 0, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2**11, False, 0, 0.7, 2000)
+
+	'''
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 3, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 4, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 5, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 6, 0.7, 2000)
+
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 3, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 4, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 5, 0.7, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 6, 0.7, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 3, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 4, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 5, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 6, 0.6, 2000)
+
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 3, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 4, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 5, 0.6, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 6, 0.6, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 3, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 4, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 5, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 6, 0.5, 2000)
+
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 3, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 4, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 5, 0.5, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 6, 0.5, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 3, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 4, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 5, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 6, 0.4, 2000)
+
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 3, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 4, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 5, 0.4, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 6, 0.4, 2000)
+
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 3, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 4, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 5, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'fpt', 45, 1, 0, True, True, 2048, False, 6, 0.3, 2000)
+	
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 3, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 4, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 5, 0.3, 2000)
+	fine_tune(batch_size, learning_rate, 'tps', 45, 1, 0, True, True, 2048, False, 6, 0.3, 2000)
+	'''
